8c8b0b08aef20a70fe6e2a772f909444f0c93c3e
axon-git <105203079+axon-git@users.noreply.github.com>
2025-01-16 18:47:33 +0200

    Update azure_users_without_mfa.sql

--- fasthttp_bruteforce_campaign/azure_users_without_mfa.sql
+++ fasthttp_bruteforce_campaign/azure_users_without_mfa.sql
@@ -2,7 +2,7 @@ SELECT DISTINCT PROPERTIES_USER_PRINCIPAL_NAME,
        IDENTITY,
        PROPERTIES:authenticationRequirement AS AuthRequirement
 FROM RAW.AZURE_SIGNIN
-WHERE EVENT_TIME > CURRENT_TIMESTAMP - INTERVAL '3 days'
+WHERE EVENT_TIME > CURRENT_TIMESTAMP - INTERVAL '30 days'
   AND AuthRequirement = 'singleFactorAuthentication'
   AND RESULT_TYPE = 0 -- only successful logins
 GROUP BY 1,2,3
